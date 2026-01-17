8f2e6cdf69a0256e8608c6502e8845f6208a8b46
Bastian Kolb-Grunder <76182879+Boisti13@users.noreply.github.com>
2026-01-17 12:29:21 +0100

    test

--- src/sprouttrack_exporter/config.py
+++ src/sprouttrack_exporter/config.py
@@ -26,7 +26,7 @@ class AppConfig:
 
     mqtt: MqttConfig = MqttConfig(host="127.0.0.1", port=1883)
 
-    base_topic: str = "sprouttracktest"
+    base_topic: str = "sprouttrack"
     ha_discovery_prefix: str = "homeassistant"
     timezone: str = "Europe/Berlin"
 
